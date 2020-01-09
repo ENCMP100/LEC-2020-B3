d6749d7e9416946c526696271e32605b44dcab9d
Kamal Ranaweera <ranaweer@ualberta.ca>
2020-01-08 23:37:15 -0700

    Added Lec 2 code file.

---
+++ lec_2_variables_and_numeric_opertors.m
@@ -0,0 +1,74 @@
+% This script demonstrates basic operations with variables and 
+% built-in functions
+
+
+% Initializing a variable with a numeric value
+age = 17;
+
+% Re-assigning another value to the same variable
+age = 18;
+
+
+% Incrementing the value of the variable
+age = age + 1;
+
+
+% Initializing two other variables
+baseDailySalary = 78.50;
+todayCommission = 7.75;
+
+% Evaluating an expression and assigning it's value to another variable
+todayPayment = baseDailySalary + todayCommission;
+
+
+% Use of more operators
+numWorkers = 12;
+
+totalLabourCost = numWorkers * todayPayment; % multiplication
+
+% Another way to do the same calculation
+totalLabourCostComputedAnotherWay = numWorkers * (baseDailySalary + todayCommission);
+
+% Let's list the details of variables that are in the workspace on the
+% command window
+whos
+
+
+% converting the age to an unsigned 8-bit integer and assigning it to
+% another variable
+age2 = uint8(age);
+
+
+% Demonstrate effects of te format command
+
+format short
+value = pi  % should show value with 4 decimal places
+
+format long
+value = pi  % should show value with more decimal places
+
+format compact
+value = pi % should produce less line spacing in the command window
+
+format loose
+value = pi % should produce more line spacing in the command window
+
+format % this brings back the default behaviour
+value = pi
+
+
+% More Numeric Operators
+a = 8;
+b = 4;
+a/b  % divided by. The result is 2
+a\b  % divided into. The result is 0.5
+a^2   % Exponent.
+-a    % Negation.
+
+
+% some basic commands
+clear a % deletes the variable a from the workspace
+clear   % clears all variables from the workspace
+exit    % closes MATLAB
+
+
