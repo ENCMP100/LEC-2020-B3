3eac49b46288fb6347e3ea3ca60716b975746aa2
Kamal Ranaweera <ranaweer@ualberta.ca>
2020-01-15 23:37:39 -0700

    Lecture 4 Code Skeletons

---
+++ Lec4/circleinfo.m
@@ -0,0 +1,20 @@
+% CIRCLEINFO: This script calculates the circumference and area of a circle
+% and display them.
+%
+% Author: YOUR NAME
+% ENCMP100 B3, Winter 2020.
+% Reference: Attaway, chapter 3.1 to 3.4
+
+
+% Radius of the circle
+radius = 2;
+
+% Calculating the circumference
+circ = 2 * pi * radius;
+
+% Calculating the area
+area = pi * radius^2;
+
+% Display the result
+circ
+area
